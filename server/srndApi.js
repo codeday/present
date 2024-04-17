a39f92d1d072495fa3526441f3b9ef15aa575480
Lola <lola@codeday.org>
2024-04-17 16:32:24 -0700

    only pull community partners with correct region (or none)

--- server/srndApi.js
+++ server/srndApi.js
@@ -64,7 +64,7 @@ export default class EventInfoApi {
   static async getCommunityPartners(event) {
     const res = await apiFetch(`query {
     cms {
-      communityPartners {
+      communityPartners(where: { OR: [{ region: { webname: "${event.webname}" } }, {regionCollection_exists:false}] }) {
         items {
           logo {
             url
